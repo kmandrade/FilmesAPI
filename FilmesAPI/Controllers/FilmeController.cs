011156819d5a42f75cdd1b2c10f4a0659bcf8235
kelvin <kelvin@PROTECH-SSC6>
2021-12-17 11:07:06 -0300

    Iniciando Construção de uma API

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FilmesAPI.Models;
+namespace FilmesAPI.Controllers
+{
+    [ApiController]//definindo nosso controlador
+    [Route("[controller]")]//definindo a rota 
+    public class FilmeController : ControllerBase
+    {
+
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]//identificando qual ação que queremos realizar criando um recurso novo no sistema
+        //post envia requisição 
+        public void AdicionarFilme([FromBody] Filme filme) //indicar que a informação vem do corpo da minha requisição Frombody
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.titulo);
+            Console.WriteLine(filme.diretor);
+
+        }
+        [HttpGet]
+        public IEnumerable<Filme> RecuperarFilmes()
+        {
+            return filmes;
+        }
+
+    }
+}
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        [Required (ErrorMessage ="O titulo do filme é obrigatorio")] //determimna que é necessário essa informacao
+        public string titulo { get; set; }
+        public string diretor { get; set; }
+
+        public string genero { get; set; }
+        [Range(1,130, ErrorMessage ="a duracao deve ter de 1 a 130 min")]
+        public int duracao { get; set; }
+
+
+    }
+}
